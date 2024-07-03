1dbac2d832e2b47f3e763b39657faa13916f9cd1
sensational03 <125127090+sensational03@users.noreply.github.com>
2024-07-03 15:33:16 +0530

    Delete theapp directory

--- theapp/socialmed/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from ..models import loginInfo
-
-class loginInfoSerializer(ModelSerializer):
-    class Meta:
-        model = loginInfo
-        fields = ('username', 'password')
--- theapp/socialmed/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class loginInfo(models.Model):
-    username = models.CharField(max_length=16)
-    password = models.CharField(max_length=18)
